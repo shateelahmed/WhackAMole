1bf494a66ccf32c34358814035c06f78f33750bb
Shateel Ahmed <shateelahmed@gmail.com>
2017-02-05 01:28:04 +0600

    Initializing project. Some bugs still present!

---
+++ js/main.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function(){
+    var score = 0;
+    function start(){
+        score = 0;
+        jQuery('.score').html('Score: ' + score);
+        jQuery('.mole').animate({'top':'0%'}, 5000, function(){
+            jQuery('.mole').animate({'top':'100%'}, 500);
+            jQuery('.score').html('GAME OVER');
+            jQuery('.score').append('<div class="try_again">TRY AGAIN</div>');
+            jQuery('.try_again').click(function(){
+                start();
+            });
+        });
+    }
+    jQuery('.mole').click(function(){
+        jQuery(this).css('background-image','url(images/hurt_mole.png)');
+        jQuery(this).stop().animate({'top':'100%'}, 500, function(){
+            score++;
+            jQuery('.score').html('Score: ' + score);
+            jQuery(this).css('background-image','url(images/happy_mole.png)');
+            jQuery(this).animate({'top':'0%'}, 5000);
+        });
+    });
+});
