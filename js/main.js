ef6fe03a5ccf6d1e50bcc100fcc399aafc10d7ac
Shateel Ahmed <shateelahmed@gmail.com>
2017-02-05 18:38:52 +0600

    game over bug fixed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 jQuery(document).ready(function(){
     var score = 0;
+    function game_over() {
+        jQuery('.mole').animate({'top':'100%'}, 500);
+        jQuery('.score').html('GAME OVER');
+        jQuery('.score').append('<div class="try_again">TRY AGAIN</div>');
+    }
     function start(){
         score = 0;
         jQuery('.score').html('Score: ' + score);
         jQuery('.mole').animate({'top':'0%'}, 5000, function(){
-            jQuery('.mole').animate({'top':'100%'}, 500);
-            jQuery('.score').html('GAME OVER');
-            jQuery('.score').append('<div class="try_again">TRY AGAIN</div>');
+            game_over();
             jQuery('.try_again').click(function(){
                 start();
             });
@@ -21,4 +24,5 @@ jQuery(document).ready(function(){
             jQuery(this).animate({'top':'0%'}, 5000);
         });
     });
+    start();
 });
